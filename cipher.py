f6d3b8ddf0a48855252155e032e3396b5d273b44
Ahmed Elsayed <101436023+AhmedBlackEye@users.noreply.github.com>
2022-10-15 23:20:15 +0100

    file formatting

--- cipher.py
+++ cipher.py
@@ -1,8 +1,10 @@
 import cryptography.fernet
 
+
 def encrypt_(msg, key=b'3gZ2d7Hz2cJXxOTJEsekj1dAB2l9Y8zGmVQp1cne_d8='):
     f = cryptography.fernet.Fernet(key)
     return f.encrypt(msg.encode())
+
 
 def decrypt_(msg, key=b'3gZ2d7Hz2cJXxOTJEsekj1dAB2l9Y8zGmVQp1cne_d8='):
     f = cryptography.fernet.Fernet(key)
--- validation.py
+++ validation.py
@@ -1,12 +1,12 @@
 from prompt_toolkit.validation import Validator, ValidationError
 
-def raise_err(msg, position):
-    raise ValidationError(
-        message=msg, 
-        cursor_position=position
-    )
 
-class NumValidator(Validator):        
+def raise_err(msg, position):
+    raise ValidationError(message=msg, cursor_position=position)
+
+
+class NumValidator(Validator):
+
     def validate(self, document):
         if not document.text.isdigit():
             raise_err("Only numbers are allowed", len(document.text))
@@ -15,33 +15,44 @@ class NumValidator(Validator):
 
 
 class UsrnameValidator(Validator):
+
     def validate(self, document):
         alphabet = 'abcdefghijklmnopqrstuvwxyz. '
         for letter in document.text:
-            if letter not in alphabet: 
-                raise_err("Only lowercase letters are allowed", len(document.text))
+            if letter not in alphabet:
+                raise_err("Only lowercase letters are allowed",
+                          len(document.text))
 
-        
+
 class DateValidator(Validator):
+
     def validate(self, document):
-        from datetime import date
+        from datetime import datey
         current_yr = date.today().year
         try:
             days, month, year = document.text.split('/')
             days, month, year = int(days), int(month), int(year)
-        except: 
+        except:
             raise_err("Enter Date in dd/mm/yyyy format", len(document.text))
-        if not (1900<year<current_yr-2 and 0<days<32 and 0<month<13):
-            raise_err(f"Year must be between 1900 and {current_yr-2} with realistic values", len(document.text))
+        if not (1900 < year < current_yr - 2 and 0 < days < 32
+                and 0 < month < 13):
+            raise_err(
+                f"Year must be between 1900 and {current_yr-2} with realistic values",
+                len(document.text))
+
 
 class PostiveIntChecker(Validator):
+
     def validate(self, document):
         if not document.text.isdigit():
             raise_err(f"You must enter a whole number", len(document.text))
 
+
 class CardNumValidator(Validator):
+
     def validate(self, document):
         if not document.text.isdigit():
             raise_err("Only numbers are allowed", len(document.text))
         elif len(document.text) != 16:
-            raise_err("Card number must contain 16 numbers", len(document.text))
+            raise_err("Card number must contain 16 numbers",
+                      len(document.text))
